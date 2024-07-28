789496a29f1cd00d2b92e13756d703dd15deb3e4
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2024-07-29 01:10:31 +0300

    Add GetX for State Management and Refactor AppBar
    - Integrated GetX package for state management across the application.
    - Replaced MaterialApp with GetMaterialApp in main.dart to enable GetX features.
    - Introduced a new widget, CirclePersonAvatarAppBar, for the app bar's leading icon to open the drawer.
    - Modified CustomHomeAppBar to use the new CirclePersonAvatarAppBar and changed the action icon to an exit icon with GetX navigation functionality.
    - Updated pubspec.yaml to include the Get package and made necessary adjustments to dependencies.

--- lib/widgets/custom_home_app_bar.dart
+++ lib/widgets/custom_home_app_bar.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:news_app_team/widgets/circle_person_avatar_app_Bar.dart';
 import 'package:news_app_team/widgets/custom_title_app_bar.dart';
 
 class CustomHomeAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -9,12 +10,17 @@ class CustomHomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       forceMaterialTransparency: true,
+      leading: const CirclePersonAvatarAppBar(),
       title: const CustomTitleAppBar(),
       centerTitle: true,
       actions: [
         IconButton(
-          onPressed: () {},
-          icon: const Icon(CupertinoIcons.search, size: 26),
+          onPressed: () => Get.back(),
+          icon: const Icon(
+            Icons.exit_to_app,
+            color: Colors.red,
+            size: 35,
+          ),
         ),
       ],
     );
