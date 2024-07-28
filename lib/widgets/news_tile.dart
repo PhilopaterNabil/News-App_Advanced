972355efd3ff70387ea1da50c659d59a4ba9dba4
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2024-07-28 23:20:06 +0300

    Add News List View and Tile Widgets
    Created `NewsListView`, `NewsListViewBuilder`, and `NewsTile` widgets to display a list of news articles. The `NewsListView` widget renders a list of articles using the `NewsTile` widget for each item. The `NewsListViewBuilder` fetches articles asynchronously and updates the UI based on the fetch status (loading, success, error). Integrated `CachedNetworkImage` for efficient image loading and `Shimmer` for loading placeholders.

---
+++ lib/widgets/news_tile.dart
@@ -0,0 +1,61 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app_team/models/article_model.dart';
+import 'package:shimmer/shimmer.dart';
+
+class NewsTile extends StatelessWidget {
+  const NewsTile({super.key, required this.articleModel});
+
+  final ArticleModel articleModel;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(6),
+          child: CachedNetworkImage(
+            imageUrl: articleModel.image ?? '',
+            placeholder: (context, url) => Shimmer.fromColors(
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.white,
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.grey,
+              ),
+            ),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          ),
+          // Image.network(
+          //   articleModel.image!,
+          //   height: 200,
+          //   width: double.infinity,
+          //   fit: BoxFit.cover,
+          // ),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        Text(
+          articleModel.title,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            color: Colors.black87,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          articleModel.subTitle ?? '',
+          maxLines: 2,
+          style: const TextStyle(color: Colors.grey, fontSize: 14),
+        )
+      ],
+    );
+  }
+}
